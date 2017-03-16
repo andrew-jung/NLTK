36362fb234de6eeefd39a25f1f86f11fb888dd3e
andrew-jung <unkofunko@gmail.com>
2017-03-16 00:45:01 -0300

    Following NLTK book

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
-import nltk
-nltk.download()
+from nltk.book import *
+from matplotlib import *
+from numpy import *
+
+def lexical_diversity(text): # How many different words are used in a text.
+    return len(text) / len(set(text))
+
+def percentage(count, total): # Calculation of how much a word count vs total count.
+    return 100 * count / total
+
+sent1 = ['Call', 'me', 'Ishmael', '.']
+print(len(sent1))
+print(lexical_diversity(sent1))
+print(percentage(text5.count("lol"), len(text5))) # Percentage of "lol"s in text5
+print('\n')
+
+list = ['Hello', 'my', 'name', 'is', 'Andrew']
+print(sorted(list))
+print(len(list))
+print(lexical_diversity(list))
+print(percentage(list.count('Andrew'), len(list)))
+
+print(text5.index('lol'))
+print(text5[121])
+print(list[4:5])
+print('\n')
+
+saying = ['After', 'all', 'is', 'said', 'and', 'done',
+          'more', 'is', 'said', 'than', 'done']
+tokens = set(saying)
+tokens = sorted(tokens)
+print(tokens[-2:])
+print('\n')
+
+fdist = FreqDist(text5) # FreqDist (frequency distribution) is the frequency of a vocabulary item in the text.
+print(fdist) # Total number of words/outcomes
+vocab1 = fdist.keys() # Distinct types in the text
+print(fdist.most_common(50)) # There is no vocab1[:50] in Py3 either typecast to list or use most_common
+print(fdist['lol']) # Returns count of string
+print('\n')
+
+# fdist.plot(50, cumulative=True)
+
+
+
+
---
+++ soupy.py
@@ -0,0 +1,2 @@
+import nltk
+from bs4 import BeautifulSoup
